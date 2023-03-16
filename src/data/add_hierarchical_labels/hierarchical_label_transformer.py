ec68fe00b055ed69fdb2a9c816c67f66920e608f
arabica <mg96272@gmail.com>
2023-03-16 22:21:04 +0100

    fashion_mnist dataset

---
+++ src/data/add_hierarchical_labels/hierarchical_label_transformer.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+class HierarchicalLabelTransformer:
+    @abstractmethod
+    def add_label(self, src: Path, dst: Path) -> None:
+        raise NotImplementedError

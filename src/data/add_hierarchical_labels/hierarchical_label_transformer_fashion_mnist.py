ce407889ac1da1eeb1af2e280f323b927c6867f8
arabica <mg96272@gmail.com>
2023-03-20 11:15:50 +0100

    news_category_dataset

--- src/data/add_hierarchical_labels/hierarchical_label_transformer_fashion_mnist.py
+++ src/data/add_hierarchical_labels/hierarchical_label_transformer_fashion_mnist.py
@@ -4,13 +4,13 @@ import pandas as pd
 import json
 
 
-class HierarchicalLabelTransformerFashionMnist():
+class HierarchicalLabelTransformerFashionMnist(HierarchicalLabelTransformer):
 
     def __init__(self):
         self.new_categories_dict = {'0': ['9', '7', '5'],  # category for "Ankle boot", "Sneaker" and "Sandal"
-                                    1: ['0', '2', '4', '6'],  # category for "T-shirt/top", "Pullover", "Coat", "Shirt"
-                                    2: ['1', '3'],  # category for "Trouser" and "Dress"
-                                    3: ['8']}  # category for "Trouser" and "Bag"
+                                    '1': ['0', '2', '4', '6'],  # category for "T-shirt/top", "Pullover", "Coat", "Shirt"
+                                    '2': ['1', '3'],  # category for "Trouser" and "Dress"
+                                    '3': ['8']}  # category for "Trouser" and "Bag"
 
     def new_label(self, row) -> str:
         for i in self.new_categories_dict:
